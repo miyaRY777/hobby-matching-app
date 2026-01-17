e48b35e915671f872bc16a2492df7b1ee5cd7a87
宮本　僚弥 <miyaRY777@gmail.com>
2026-01-17 17:54:49 +0900

    feat: Deviseのsign_upでnicknameを許可する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # devise 追加
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # 新規登録時
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
