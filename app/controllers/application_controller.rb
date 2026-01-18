3140d5323a66b466e8d2a5a0f9ace103f04b82e1
宮本　僚弥 <miyaRY777@gmail.com>
2026-01-18 11:38:16 +0900

    登録成功時にリダイレクト設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,16 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # 新規登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  # ログイン後も同じ場所に飛ばしたい場合（任意）
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   def configure_permitted_parameters
     # 新規登録時
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
