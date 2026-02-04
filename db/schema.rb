db12efecf6080919cc9a3e91e14327ab085664ba
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-04 21:12:05 +0900

    feat: add Hobby model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_20_054224) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_04_120347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id", null: false
