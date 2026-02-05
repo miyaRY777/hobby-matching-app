dd14a1a0970aea5ef9d0700bcae8872f77f4a8c7
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-05 15:38:34 +0900

    feat: add Profile-Hobby associations via profile hobbies

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
   validates :user_id, uniqueness: true
   validates :bio, presence: true, length: { maximum: 500 }
+
+  has_many :profile_hobbies, dependent: :destroy
+  has_many :hobbies, through: :profile_hobbies
 end
--- app/models/profile_hobby.rb
+++ app/models/profile_hobby.rb
@@ -1,4 +1,6 @@
 class ProfileHobby < ApplicationRecord
   belongs_to :profile
   belongs_to :hobby
+
+  validates :hobby_id, uniqueness: {scope: :profile_id}
 end
