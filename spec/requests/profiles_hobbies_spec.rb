648ecaeab662551694ec74f863f97bda181b2429
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-10 00:02:44 +0900

    test: sign in user and prepare current_user profile in request specs

--- spec/requests/profiles_hobbies_spec.rb
+++ spec/requests/profiles_hobbies_spec.rb
@@ -1,10 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "Profile hobbies", type: :request do
+  let(:user)    { create(:user) }
+  let!(:profile) { create(:profile, user:) } # ← current_user の profile を明示
+
+  before do
+    sign_in user
+  end
+
   describe "更新：PATCH /profile (my_profile_path)" do
     it "タグ入力を受け取り、hobbies を全置換して表示に反映される" do
-      profile = create(:profile)
-
       patch my_profile_path, params: {
         profile: { hobbies_text: "rails, ruby" }
       }
@@ -17,11 +22,9 @@ RSpec.describe "Profile hobbies", type: :request do
     end
 
     it "バリデーション失敗時に入力値を保持して再描画される" do
-      profile = create(:profile)
-
       patch my_profile_path, params: {
         profile: {
-          nickname: "",
+          bio: nil,
           hobbies_text: "rails, ruby"
         }
       }
@@ -33,9 +36,9 @@ RSpec.describe "Profile hobbies", type: :request do
 
   describe "編集：GET /profile/edit (edit_my_profile_path)" do
     it "編集画面で既存の趣味がカンマ区切りで初期表示される" do
-      profile = create(:profile)
       rails = create(:hobby, name: "rails")
       ruby  = create(:hobby, name: "ruby")
+
       create(:profile_hobby, profile:, hobby: rails)
       create(:profile_hobby, profile:, hobby: ruby)
 
@@ -45,6 +48,5 @@ RSpec.describe "Profile hobbies", type: :request do
     end
   end
 end
-
 
 # frozen_string_literal: true まだ必要性がわかっていない
