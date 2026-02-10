14306b1cbc1b71115dc31279bf3ef4b0ac76f7f5
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-10 14:32:47 +0900

    test: add system spec for profile hobby update flow

---
+++ spec/system/profile_hobbies_flow_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "趣味(タグ)登録の一連の流れ", type: :system do
+  it "ログインして、プロフィール編集でタグを更新し、詳細に表示させる" do
+    user = create(:user)
+    create(:profile, user: user)
+
+    visit new_user_session_path
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    profile = user.profile
+    visit edit_my_profile_path
+
+    fill_in "タグ", with: "rails, ruby"
+    click_button "更新する"
+
+    expect(page).to have_content("rails")
+    expect(page).to have_content("ruby")
+  end
+end
