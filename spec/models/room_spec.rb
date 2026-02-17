9dce51a42a6a3f890df43be06d6d86c8b1367c90
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-18 00:22:28 +0900

    test: add association spec for Room model (RED)

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,9 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Room, type: :model do
-  it "発行者がプロフィール（issuer_profile）がない場合は無効にする" do
+  it "発行者プロフィール（issuer_profile）がない場合は無効にする" do
     room = described_class.new(issuer_profile: nil)
 
     expect(room).not_to be_valid
+  end
+
+  it "has associations" do
+    room = described_class.reflect_on_association(:issuer_profile)
+    
+    expect(room.macro).to eq(:belongs_to)
+    expect(described_class.reflect_on_association(:room_memberships).macro).to eq(:has_many)
+    expect(described_class.reflect_on_association(:profiles).macro).to eq(:has_many)
+    expect(described_class.reflect_on_association(:share_link).macro).to eq(:has_one)
   end
 end
