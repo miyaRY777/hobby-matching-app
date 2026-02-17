715ff16f7d509680f83d7db2b1cc9931dab99328
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-18 00:27:18 +0900

    feat: add associations to Room model (issuer_profile, memberships, share_link) (GREEN)

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :issuer_profile, class_name: "Profile"
+
+  has_many :room_memberships, dependent: :destroy
+  has_many :profiles, through: :room_memberships
+
+  has_one :share_link, dependent: :destroy
 end
