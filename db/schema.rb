503a266a5c4f377d4a0f2b02fd5c41d3f66a3497
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-17 22:42:04 +0900

    feat: add RoomMembership model with DB and model constraints(GREEN)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_17_062327) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_17_130019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_17_062327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
+  end
+
+  create_table "room_memberships", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "profile_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_room_memberships_on_profile_id"
+    t.index ["room_id", "profile_id"], name: "index_room_memberships_on_room_id_and_profile_id", unique: true
+    t.index ["room_id"], name: "index_room_memberships_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -73,6 +83,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_17_062327) do
   add_foreign_key "profile_hobbies", "hobbies"
   add_foreign_key "profile_hobbies", "profiles"
   add_foreign_key "profiles", "users"
+  add_foreign_key "room_memberships", "profiles"
+  add_foreign_key "room_memberships", "rooms"
   add_foreign_key "rooms", "profiles", column: "issuer_profile_id"
   add_foreign_key "share_links", "rooms"
 end
