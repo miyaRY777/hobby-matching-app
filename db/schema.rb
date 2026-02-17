218425067e0ea40d47ffbf894f45411a748c75fd
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-17 10:49:20 +0900

    feat: create rooms table and Room model with issuer_profile foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_04_141843) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_17_004659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,14 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_04_141843) do
     t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
   end
 
+  create_table "rooms", force: :cascade do |t|
+    t.bigint "issuer_profile_id", null: false
+    t.string "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["issuer_profile_id"], name: "index_rooms_on_issuer_profile_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,4 +63,5 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_04_141843) do
   add_foreign_key "profile_hobbies", "hobbies"
   add_foreign_key "profile_hobbies", "profiles"
   add_foreign_key "profiles", "users"
+  add_foreign_key "rooms", "profiles", column: "issuer_profile_id"
 end
---
+++ spec/factories/rooms.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :room do
+    issuer_profile { nil }
+    label { "MyString" }
+  end
+end
