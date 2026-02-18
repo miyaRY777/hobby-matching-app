5f4e0cbfd5df4dcc46a56fdb5eceb18ae62b1158
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-18 23:43:58 +0900

    test: add request spec for joining room via share link (RED)

---
+++ spec/requests/shares_show_join_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Shares#show", type: :request do
+  it "共有リンクが有効で、閲覧者にプロフィールがあるとき、room に参加する" do
+
+    # 共有リンクを用意
+    issuer = create(:profile)
+    room = create(:room, issuer_profile: issuer)
+    share_link = create(:share_link, room: room, expires_at: 24.hour.from_now)
+
+    # 閲覧者のプロフィールを用意
+    viewer_user = create(:user)
+    viewer_profile = create(:profile, user: viewer_user)
+
+    sign_in viewer_user
+
+    expect {
+      get share_path(share_link.token)
+    }.to change(RoomMembership, :count).by(1)
+
+    membership = RoomMembership.order(:create_at).last
+    expect(membership.room).to eq(room)
+    expect(membership.profile).to eq(viewer_profile)
+  end
+end
