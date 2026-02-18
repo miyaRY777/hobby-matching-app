c02b36e54cc1a1a23f331b1bfe6459b3fcb6be16
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-19 00:06:57 +0900

    feat: join room on share page access when viewer has profile (GREEN)

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,6 +2,12 @@ class SharesController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    share_link = ShareLink.find_by!(token: params[:token])
+
+    room = share_link.room
+    viewer_profile = current_user.profile
+
+    RoomMembership.find_or_create_by!(room: room, profile: viewer_profile) if viewer_profile
     head :ok
   end
 end
