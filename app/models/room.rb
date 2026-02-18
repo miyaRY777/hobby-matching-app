5f72e5fd2583e85ea5d6de54f2618c97de82becc
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-18 11:42:31 +0900

    feat: implement room-related associations for Profile and Room (GREEN)

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  belongs_to :issuer_profile, class_name: "Profile"
+  belongs_to :issuer_profile, class_name: "Profile", foreign_key: :issuer_profile_id, inverse_of: :issued_rooms
 
   has_many :room_memberships, dependent: :destroy
   has_many :profiles, through: :room_memberships
