746c709f97875467a0e1b06e3afc600db4455133
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-19 17:39:49 +0900

    feat: show share room members in join order (GREEN)

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -5,10 +5,13 @@ class SharesController < ApplicationController
     share_link = ShareLink.find_by!(token: params[:token])
     return head :gone if share_link.expires_at <= Time.current
 
-    room = share_link.room
+    @room = share_link.room
     viewer_profile = current_user.profile
 
-    RoomMembership.find_or_create_by!(room: room, profile: viewer_profile) if viewer_profile
+    RoomMembership.find_or_create_by!(room: @room, profile: viewer_profile) if viewer_profile
 
+  @memberships = @room.room_memberships
+                      .includes(profile: :user)
+                      .order(created_at: :asc)
   end
 end
