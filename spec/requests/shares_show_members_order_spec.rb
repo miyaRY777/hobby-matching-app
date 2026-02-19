ceee67f1eb8f98fbaad1aa43454861c9f77f1e79
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-19 17:27:44 +0900

    test: fix share page member order request spec (RED)

--- spec/requests/shares_show_members_order_spec.rb
+++ spec/requests/shares_show_members_order_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "shares#show", type: :request do
   it "メンバーを参加した順番で表示する" do
     # 発行者Aと共有リンクを作成する
-    issuer_user = create(:user, nickname: "A")
+    issuer_user = create(:user, nickname: "tana_A")
     issuer_profile = create(:profile, user: issuer_user)
     room = create(:room, issuer_profile: issuer_profile)
     share_link = create(:share_link, room: room, expires_at: 1.hour.from_now)
@@ -12,9 +12,12 @@ RSpec.describe "shares#show", type: :request do
     create(:room_membership, room: room, profile: issuer_profile)
 
      # 参加者B, C, D を作成する
-    b_user = create(:user, nickname: "B")
-    c_user = create(:user, nickname: "C")
-    d_user = create(:user, nickname: "D")
+    b_user = create(:user, nickname: "tana_B")
+    b = create(:profile, user: b_user)
+    c_user = create(:user, nickname: "tana_C")
+    c = create(:profile, user: c_user)
+    d_user = create(:user, nickname: "tana_D")
+    d = create(:profile, user: d_user)
 
     # JOIN順を作る（B → C → D）
     sign_in b_user
@@ -39,9 +42,9 @@ RSpec.describe "shares#show", type: :request do
     body = response.body
 
     # 参加順で表示されていることを確認
-    expect(body).to include("A", "B", "C", "D")
-    expect(body.index("A")).to be < body.index("B")
-    expect(body.index("B")).to be < body.index("C")
-    expect(body.index("C")).to be < body.index("D")
+    expect(body).to include("tana_A", "tana_B", "tana_C", "tana_D")
+    expect(body.index("tana_A")).to be < body.index("tana_B")
+    expect(body.index("tana_B")).to be < body.index("tana_C")
+    expect(body.index("tana_C")).to be < body.index("tana_D")
   end
 end
