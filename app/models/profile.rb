ec4be2e130babe81749b7d8f112fba25b8fe512b
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-20 14:31:48 +0900

    feat: cascade delete rooms when profile is destroyed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,7 +7,7 @@ class Profile < ApplicationRecord
   has_many :hobbies, through: :profile_hobbies
   has_many :room_memberships, dependent: :destroy
   has_many :joined_rooms, through: :room_memberships, source: :room
-  has_many :issued_rooms, class_name: "Room", foreign_key: :issuer_profile_id, inverse_of: :issuer_profile, dependent: :nullify
+  has_many :issued_rooms, class_name: "Room", foreign_key: :issuer_profile_id, inverse_of: :issuer_profile, dependent: :destroy
 
   attr_accessor :hobbies_text
 
