f00ea01d17b1d4ede02a5e162ab0ab85d88fe3fd
宮本　僚弥 <miyaRY777@gmail.com>
2026-02-20 21:55:15 +0900

    feat: add edit/update for room label in my share links (GREEN)

--- app/controllers/my/share_links_controller.rb
+++ app/controllers/my/share_links_controller.rb
@@ -1,9 +1,13 @@
 class My::ShareLinksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_room, only: %i[edit update]
 
   def index
     profile = current_user.profile
     @rooms = profile ? profile.issued_rooms.includes(:share_link) : Room.none
+  end
+
+  def edit
   end
 
   # あとで、Service Objectに切り出す
@@ -24,5 +28,18 @@ class My::ShareLinksController < ApplicationController
   end
 
   def update
+    @room.update!(room_params)
+    redirect_to my_share_links_path, notice: "部屋名を更新しました"
+  end
+
+  private
+
+  def set_room
+    # 発行者だけが編集できるように絞る
+    @room = current_user.profile.issued_rooms.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:label)
   end
 end
