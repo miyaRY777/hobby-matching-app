3d1d046d7ae66e361805cd6850cfd8a2b34ac56e
宮本　僚弥 <miyaRY777@gmail.com>
2026-03-12 17:56:35 +0900

    feat:   profile_hobbiesにdescriptionカラムを追加

--- app/models/profile_hobby.rb
+++ app/models/profile_hobby.rb
@@ -3,4 +3,5 @@ class ProfileHobby < ApplicationRecord
   belongs_to :hobby
 
   validates :hobby_id, uniqueness: { scope: :profile_id }
+  validates :description, length: { maximum: 200 }, allow_blank: true
 end
